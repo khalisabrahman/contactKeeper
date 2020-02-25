b0bb638dee1967547ca2fce30fdb8a8349c2251b
Khalis Ab Rahman <khalisabrahman@gmail.com>
2020-02-25 19:50:18 +0800

    Contacts and ContactItem Components

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -39,7 +39,7 @@ const ContactState = props => {
         ]
     };
 
-    const [state,dispatch] = useReducer(contactReducer, initalState);
+    const [state,dispatch] = useReducer(contactReducer, initialState);
 
     // Add Contact
 
